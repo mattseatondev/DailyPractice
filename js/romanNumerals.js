8bf54c95143f19553cb1d1ebb10519bb37f41694
mattseatondev <mattseatondev@gmail.com>
2023-08-12 09:29:03 -0400

    good thing I don't think in latin

---
+++ js/romanNumerals.js
@@ -0,0 +1,23 @@
+const romanize = roman => {
+    const numerals = [
+        ['I', 1],
+        ['V', 5],
+        ['X', 10],
+        ['L', 50],
+        ['C', 100],
+        ['D', 500],
+        ['M', 1000]
+    ];
+    const findNum = letter => numerals.find(numSet => numSet[0] == letter)[1];
+    let num = 0;
+    roman = roman.split('');
+    roman.forEach((r, rIndex) => {
+        if (rIndex == roman.length - 1) {
+            num += findNum(r);
+        }
+        // if (r == roman[rIndex + 1])
+    });
+    return num;
+}
+
+console.log(romanize('XXII'));
